c0f426c40a14142a3e3c2c09ee348264748b0217
Aluisio Pereira <oaluiser@gmail.com>
2024-09-05 20:13:59 -0300

    Adiciona orders

---
+++ site/app/api/orders/route.tsx
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+  const orders = await prisma.order.findMany()
+  return Response.json(orders)
+}
+
+export async function POST(request: Request) {
+  const body = await request.json()
+
+  const order = await prisma.order.create({
+    data: {
+      userId: body.userId,
+      products: {
+        create: body.products.map((product: any) => ({
+          quantity: product.quantity,
+          product: { connect: { id: product.productId } }
+        }))
+      }
+    }
+  })
+
+  return Response.json(order)
+}
+
+export async function DELETE(request: Request) {
+  const body = await request.json()
+
+  await prisma.order.delete({ where: { id: body.id } })
+
+  return Response.json(body)
+}
